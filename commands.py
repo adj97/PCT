bb1e820debaaf0afe968ba787e2ebf8b81f8f8de
Andrew Dixon <andrew.dixon@capgemini.com>
2021-11-26 14:24:52 +0000

    fct: readme and main

--- commands.py
+++ commands.py
@@ -20,12 +20,12 @@ def new(args):
 
 def process(node, root):
     for obj in node:
-        if type(obj["content"]) == list:
-            folder_name = "/".join([root,
-                                   obj["name"].format(new_project_name)])
+        if type(obj["content"][0]) == dict:
+            folder_name = "/".join([root, obj["name"].format(new_project_name)])
             mkdir(folder_name)
             process(obj["content"], folder_name)
-        elif type(obj["content"] == str):
+        elif type(obj["content"][0] == str):
             file_name = "/".join([root, obj["name"]])
             f = open(file_name, "x")
-            f.write(obj["content"].format(new_project_name))
+            #print("\n".join(obj["content"]))
+            f.write("\n".join(obj["content"]).format(new_project_name))
