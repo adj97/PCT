e48926d0f4400e39eebd5d50a90a4988dd176bc4
Andrew Dixon <andrew.dixon@capgemini.com>
2021-11-26 14:32:04 +0000

    fct: commands

--- mycode/helpers.py
+++
@@ -1,9 +0,0 @@
-output_types={
-    "o":" output",
-    "e":"  error",
-    "h":"   help"
-}
-
-def output(type, output):
-    output = ["mycode", output_types[type], str(output)]
-    print(": ".join(output))
